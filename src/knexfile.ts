530cb84f53a90f3701ceadf309177cda4a3ed2f4
Gabriel Silva <gabriel127.gk@gmail.com>
2022-12-28 16:28:18 -0300

    Ageitado a hora do bd - pt -4

--- src/knexfile.ts
+++ src/knexfile.ts
@@ -5,14 +5,20 @@ const configKnexDatabase = {
   development: {
     client: "pg",
     connection: env.DATABASE_URL,
+    charset  : "utf8",
+    timezone: "UTC",
     searchPath: ["knex", "public"],
       migrations: {
          tableName: "knex_migrations",
          directory: path.resolve(`src/data/migrations`),
          extension: "ts",
       },
-      timezone: "utc",
       pool: {
+        afterCreate: function(connection: any, callback: any) {
+          connection.query('SET time_zone = timezone;', function(err: any) {
+            callback(err, connection);
+          });
+        },
          min: 2,
          max: 10,
       },
