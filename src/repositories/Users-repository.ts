f18c2f0e12228b54802dd0daa92dfb6ce0739c8b
Gabriel Silva <gabriel127.gk@gmail.com>
2022-12-29 01:51:15 -0300

    Ageitando horas para o bd da nuvem - 3.

--- src/repositories/Users-repository.ts
+++ src/repositories/Users-repository.ts
@@ -30,8 +30,8 @@ implements IUsersModel {
     .insert({
       ...data, 
       verify: false,
-      created_at: new Date() + "UTC",
-      updated_at: new Date() + "UTC"
+      created_at: new Date().setHours(new Date().getHours() - 3),
+      updated_at: new Date().setHours(new Date().getHours() - 3),
     });
   };
 
