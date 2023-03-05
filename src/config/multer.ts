a1082573df5ec1812be1edd6b21141ed2492ed4b
Gabriel Silva <gabriel127.gk@gmail.com>
2023-03-05 17:24:11 -0300

    Add multer-s3 - 3

--- src/config/multer.ts
+++ src/config/multer.ts
@@ -9,8 +9,8 @@ import path from "path";
 const s3 = new S3Client({
   region: "us-east-1",
   credentials: {
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
+    accessKeyId: process.env.AWS_ACCESS_KEY!,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS!,
   },
 });
 
